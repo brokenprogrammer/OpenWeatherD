2357a1d77c5a03ec7e4aff48783f79dd031f7f2b
Oskar Mendel <brokenprogrammer@gmail.com>
2017-06-21 22:48:15 +0200

    Added documentation to options.

--- source/Options.d
+++ source/Options.d
@@ -49,48 +49,103 @@ class Options {
         return Options.instance;
     }
 
+    /**
+	 * Returns the set API key or an empty string if its not set.
+	 *
+	 * Return: An API key value.
+	 */
     public string getKey() {
         return (key !is null)
             ? this.key
             : "";
     }
 
+    /**
+	 * Returns the set API language value or an english if its not set.
+	 *
+	 * Return: An API language value.
+	 */
     public Languages getLanguage() {
         return this.language;
     }
 
+    /**
+	 * Returns the set API units or Kelvin if its not set.
+	 *
+	 * Return: An API units value.
+	 */
     public Units getUnits() {
         return this.unit;
     }
 
+    /**
+	 * Returns the set timeout or 4096 if its not set.
+	 *
+	 * Return: An API timeout value.
+	 */
     public int getTimeout() {
         return (this.timeout != 0)
             ? this.timeout
             : this.TIMEOUT_DEFAULT;
     }
 
+    /**
+	 * Returns the set API attempts or 3 if its not set.
+	 *
+	 * Return: An API attempts value.
+	 */
     public int getAttempts() {
         return (this.attempts != 0)
             ? this.attempts
             : this.ATTEMPTS_DEFAULT; 
     }
 
+    /**
+	 * Sets the API key value.
+	 * 
+	 * Params:
+	 *		key =  An API key value.
+	 */
     public void setKey(string key) {
         this.key = key;
     }
 
+    /**
+	 * Sets the API language value.
+	 * 
+	 * Params:
+	 *		language =  An API language value.
+	 */
     public void setLanguage(Languages language) {
         this.language = language;
     }
 
+    /**
+	 * Sets the API unit value.
+	 * 
+	 * Params:
+	 *		unit =  An API unit value.
+	 */
     public void setUnits(Units unit) {
         this.unit = unit;
     }
 
+    /**
+	 * Sets the API timeout value.
+	 * 
+	 * Params:
+	 *		timeout =  An API timeout value.
+	 */
     public void setTimeout(int timeout) {
         this.timeout = timeout;
     }
 
+    /**
+	 * Sets the API attempts value.
+	 * 
+	 * Params:
+	 *		attempts =  An API attempts value.
+	 */
     public void setAttempts(int attempts) {
         this.attempts = attempts;
     }
